115a1ccf274deef303a1cdf6804c2eec6c9fdbf1
erentpcu <eren_tpcu@hotmail.com>
2023-05-18 01:27:43 +0300

    Revert "Revert "PROFİLİ YAPTIM AMA FİREBASE E ENTEGRE EDİLECEK.""
    This reverts commit a1b75121a46178403ac4e8be286ff2e15a5f0089.

--- lib/PartyDetails.dart
+++ lib/PartyDetails.dart
@@ -1,14 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:guestapp380/CreateParty.dart';
+import 'package:guestapp380/ProfilePage.dart';
+import 'package:guestapp380/PartyDetails.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
 
 class PartyDetails extends StatelessWidget {
 
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-          child: Text(
-              "PARTY DETAILS"
-          ),
-        )
-    );
+        body: buildCoverImage(),
+
+
+
+      );
+
   }
 }
+
+Widget buildCoverImage() => Container(
+  color:Colors.grey,
+  child:Image.network('https://as2.ftcdn.net/v2/jpg/01/20/87/67/1000_F_120876722_dJlNWgyMJxnhLzuA6e9cKRypSECZVQjy.jpg',
+  width:double.infinity,
+  height: 280,
+      fit: BoxFit.cover,)
+);
--- lib/ProfilePage.dart
+++ lib/ProfilePage.dart
@@ -1,14 +1,115 @@
 import 'package:flutter/material.dart';
 
-class ProfilePage extends StatelessWidget {
+class ProfilePage extends StatefulWidget {
+  bool isObscurePassword = true;
+  @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
 
+class _ProfilePageState extends State<ProfilePage> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-          child: Text(
-              "PROİFLE PAGE"
-          ),
-        )
+      appBar: AppBar(
+        title: Text("Your Profile"),
+
+      ),
+      body: Container(
+        padding: EdgeInsets.only(left: 15, top: 20, right: 15),
+        child: GestureDetector(
+            onTap: () {
+              FocusScope.of(context).unfocus();
+            },
+            child: ListView(
+              children: [
+                Center(
+                  child: Stack(
+                    children: [
+                      Container(
+                        width: 130,
+                        height: 130,
+                        decoration: BoxDecoration(
+                          border: Border.all(width: 4, color: Colors.white),
+                          boxShadow: [
+                            BoxShadow(
+                              spreadRadius: 2,
+                              blurRadius: 10,
+                              color: Colors.black.withOpacity(0.1),
+                            ),
+                          ],
+                          shape: BoxShape.circle,
+                          image: DecorationImage(
+                            fit: BoxFit.cover,
+                            image: NetworkImage(
+                                "https://cdn.pixabay.com/photo/2014/04/02/10/25/man-303792_960_720.png"), //BURAYA FİREBASE OLAYLARI-------------------
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 5),
+                buildTextField("Full Name", "Enter your Full Name", false),
+                buildTextField("Age", "Your Age", false),
+                buildTextField("E-mail", "example@hotmail.com", false),
+                buildTextField("Password", "***********", true),
+                buildTextField("Location", "Your Location", false),
+                SizedBox(height: 5),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    OutlinedButton(
+                      onPressed: () {},
+                      child: Text(
+                        "CANCEL",
+                        style: TextStyle(
+                          fontSize: 15,
+                          letterSpacing: 2,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                    ElevatedButton(
+                        onPressed: () {},
+                        child: Text(
+                          "SAVE",
+                          style: TextStyle(
+                            fontSize: 15,
+                            letterSpacing: 2,
+                            color: Colors.white,
+                          ),
+                        )),
+                  ],
+                ),
+              ],
+            )),
+      ),
+    );
+  }
+
+  Widget buildTextField(
+      String labelText, String placeholder, bool isPasswordTextField) {
+    return Padding(
+      padding: EdgeInsets.only(bottom: 30),
+      child: TextField(
+        obscureText: isPasswordTextField ? true : false,
+        decoration: InputDecoration(
+            suffixIcon: isPasswordTextField
+                ? IconButton(
+                    icon: Icon(Icons.remove_red_eye, color: Colors.grey),
+                    onPressed: () {},
+                  )
+                : null,
+            contentPadding: EdgeInsets.only(bottom: 5),
+            labelText: labelText,
+            floatingLabelBehavior: FloatingLabelBehavior.always,
+            hintText: placeholder,
+            hintStyle: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+              color: Colors.grey,
+            )),
+      ),
     );
   }
 }
